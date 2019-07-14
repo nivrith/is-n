5f6f58b64295049ba1f87a4203302dea7c9c803e
Nivrith <nivrithgomatam@gmail.com>
2019-07-14 21:22:16 +0930

    Fix overloads

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,16 @@
  * Licensed under the MIT License.
  */
 
-import {nIsFinite} from 'n-is-finite';
+import {
+  nIsFinite
+} from 'n-is-finite';
 
-export const isN = (n: any) => {
+
+export function isN( n: number ): boolean;
+export function isN( n: string ): boolean;
+export function isN( n: any ) {
   const type = typeof n;
-  if (type === 'number') return n - n === 0
-  if (type === 'string' && n.trim() !== '') return Number.isFinite ? Number.isFinite(+n) : isFinite(+n)
+  if ( type === 'number' ) return n - n === 0
+  if ( type === 'string' && n.trim() !== '' ) return Number.isFinite ? Number.isFinite( +n ) : isFinite( +n )
   return false;
 };
